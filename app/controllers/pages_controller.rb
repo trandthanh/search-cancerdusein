ece85a76a504539e4605782564e7b75326c33e62
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-11-29 18:14:17 +0100

    seeded first seed of joint table category & implemented search over Category model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   def home
     # @search = Link.search(params.fetch(:query, "*"))
-    @search = Link.search params[:query], operator: "or"
+    @link_search = Link.search params[:query], operator: "or"
+    @theme_search = Theme.search params[:query], operator: "or"
   end
 
   def search
     # @search = Link.search(params.fetch(:query, "*"))
-    @search = Link.search params[:query], operator: "or"
+    @link_search = Link.search params[:query], operator: "or"
+    @theme_search = Theme.search params[:query], operator: "or"
   end
 end
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,4 +2,6 @@ class Theme < ApplicationRecord
   has_many :categories
 
   validates :title, presence: true, inclusion: { in: %w(s'informer gerer vivre ressources recherche) }
+
+  searchkick match: :word_middle
 end
