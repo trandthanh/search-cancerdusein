9cbcc07e85a131920227e8dec1a68e5a602ee76e
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-12-02 23:06:14 +0100

    added focus and reset value to search input in navbar

--- app/javascript/components/search.js
+++ app/javascript/components/search.js
@@ -1,9 +1,12 @@
 function searchBar() {
   const searchIcon = document.querySelector('.search-button-js');
   const searchBarBig = document.querySelector('.searchbar-medium');
+  const input = document.querySelector('input');
   searchIcon.addEventListener("click", (event) => {
     event.preventDefault();
+    searchBarBig.value = "";
     searchBarBig.classList.toggle("searchbar-medium-wide");
+    searchBarBig.focus();
   });
 }
 
