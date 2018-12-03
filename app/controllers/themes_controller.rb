98b09f41199df313aec7208c5ddd988cdb45fcec
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-12-03 23:23:03 +0100

    added keywords filter for results by theme

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -6,5 +6,7 @@ class ThemesController < ApplicationController
     @livre_formatted_links = Link.where(format: "livre").joins(:categories).joins(:themes).where(themes: { title: @theme.title })
     @article_formatted_links = Link.where(format: "article").joins(:categories).joins(:themes).where(themes: { title: @theme.title })
     @information_formatted_links = Link.where(format: "information").joins(:categories).joins(:themes).where(themes: { title: @theme.title })
+
+    @keywords = Keyword.all
   end
 end
