8d365874c250f1a2384cea4e97d721ed547fd841
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-12-03 20:11:43 +0100

    implemented back for themes show

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,5 +2,9 @@ class ThemesController < ApplicationController
   def show
     @themes = Theme.all
     @theme = Theme.find(params[:id])
+
+    @livre_formatted_links = Link.where(format: "livre").joins(:categories).joins(:themes).where(themes: { title: @theme.title })
+    @article_formatted_links = Link.where(format: "article").joins(:categories).joins(:themes).where(themes: { title: @theme.title })
+    @information_formatted_links = Link.where(format: "information").joins(:categories).joins(:themes).where(themes: { title: @theme.title })
   end
 end
